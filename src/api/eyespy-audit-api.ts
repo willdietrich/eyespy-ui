4d7c05d5f8f865173e03359413e6f6e280d8f64f
Will Dietrich <will.a.dietrich@gmail.com>
2024-04-07 21:44:23 -0500

    Connect to audit API.

--- src/api/eyespy-audit-api.ts
+++ src/api/eyespy-audit-api.ts
@@ -1,7 +1,17 @@
 
-export const GetAuditLogsByLookback = async () => {
-    const url = "http://127.0.0.1:8000/";
-    const response = await fetch(url);
+export const GetAuditLogsByLookback = async (timeframe: number) => {
+    const url = "http://localhost:8000/audit/search";
+    const requestBody = {
+        type: 0,
+        timeframe_days: timeframe
+    }
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+    });
 
     if (!response.ok) {
         throw new Error('Failed to retrieve URL' + url);
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -5,12 +5,12 @@ import { GetAuditLogsByLookback } from "../api/eyespy-audit-api";
 export function Index() {
   const { isPending, isError, data, error } = useQuery({
     queryKey: ['lookback-timeframe'],
-    queryFn: GetAuditLogsByLookback
+    queryFn: () => GetAuditLogsByLookback(30)
   });
 
   if (isPending) {
     return (
-      <Typography>Loading</Typography>
+      <Typography>Loading...</Typography>
       );
   }
 
@@ -24,11 +24,32 @@ export function Index() {
       );
   }
 
+  interface AuditResult {
+    channel: string,
+    channel_id: number,
+    user: string,
+    user_id: number,
+    join_time: string,
+    leave_time: string
+  }
+
+  const renderResults = (results: AuditResult[]) => {
+    return results.map((result) => {
+      return (
+        <>
+          <Typography variant="h3">{result.channel}:{result.channel_id}</Typography>
+          <Typography variant="body1">{result.user}:{result.user_id}</Typography>
+          <Typography variant="body1">{result.join_time}:{result.leave_time}</Typography>
+        </>
+      )
+    });
+  };
+
   return (
     <>
       <Typography variant="h1">Index</Typography>
       <Typography variant="h2">Server Return:</Typography>
-      <Typography variant="body1">{data.message}</Typography>
+      {renderResults(data.results)}
     </>
   );
 }
